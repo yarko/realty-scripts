6eb0b6a4a31d421d0f741e44b645d076c3bb1fcf
Yarko Tymciurak <yarkot1@gmail.com>
2020-09-25 14:59:02 -0500

    update paperfix: don't add blank lines before the first line

--- paperfix.sh
+++ paperfix.sh
@@ -2,7 +2,9 @@
 
 sed -e '
 # insert blank line before any md line starting w/ header '#' or checkbox
-/^[[#]/ i
+2,$ {
+    /^[[#]/ i
 
+}
 # 8-space bullits? turn into 4-space:
 s/^[ ]\{8\}-/    -/'
